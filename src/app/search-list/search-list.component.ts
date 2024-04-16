5db91f4aad06bbd892976487cdec7958bac35147
Robert <robertflan02@gmail.com>
2024-04-16 15:12:01 +0100

    basic search function

--- src/app/search-list/search-list.component.ts
+++ src/app/search-list/search-list.component.ts
@@ -1,18 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-user-list',
+  selector: 'app-search-list',
   templateUrl: './search-list.component.html',
   styleUrls: ['./search-list.component.css']
 })
 export class SearchListComponent implements OnInit {
-  usernames$: Observable<any[]> = new Observable<any[]>();
+  searchQuery: string = ''; 
+  searchResults: string[] = [];
+  usernames: string[] = []; 
 
-  constructor(private firestore: AngularFirestore) { }
+  constructor(
+    private route: ActivatedRoute,
+    private firestore: AngularFirestore
+  ) { }
 
   ngOnInit() {
-    this.usernames$ = this.firestore.collection('users').valueChanges();
+  this.route.queryParams.subscribe(params => {
+    this.searchQuery = params['query'] || ''; 
+    console.log('Search query:', this.searchQuery); 
+    this.fetchUsernames(); 
+  });
+}
+
+  fetchUsernames() {
+  this.firestore.collection('users').valueChanges().subscribe(
+  (usernames: any[]) => {
+    console.log('Usernames:', usernames); 
+    this.usernames = usernames.map(user => user.userName); 
+    this.filterSearchResults(); 
+  },
+  error => {
+    console.error('Error fetching usernames:', error);
   }
+);
+}
+
+ filterSearchResults() {
+  if (this.usernames && this.usernames.length > 0) {
+    this.searchResults = this.usernames.filter(username =>
+      username && username.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
+  } else {
+    this.searchResults = [];
+  }
+}
 }
